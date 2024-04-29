58a3ae7c0018efba7d6bb0f281df9f5c18eb9085
juanm512 <512juanm@gmail.com>
2024-04-29 19:03:35 -0300

    ej04 terminado

--- target/debug/deps/practica-f7e7f87b40f7ced6.d
+++ target/debug/deps/practica-f7e7f87b40f7ced6.d
@@ -1,7 +1,13 @@
-C:\Users\512ju\Desktop\facultad\INFORMATICA-APU\2024\Rust\practica\target\debug\deps\practica-f7e7f87b40f7ced6.exe: src\main.rs src\entregable_1\mod.rs src\entregable_1\juan_manuel_vila_v1.rs
+C:\Users\512ju\Desktop\facultad\INFORMATICA-APU\2024\Rust\practica\target\debug\deps\practica-f7e7f87b40f7ced6.exe: src\main.rs src\tp03\mod.rs src\tp03\ej01.rs src\tp03\ej02.rs src\tp03\ej03.rs src\tp03\ej04.rs src\tp03\ej05.rs src\tp03\ej06.rs src\tp03\ej07.rs
 
-C:\Users\512ju\Desktop\facultad\INFORMATICA-APU\2024\Rust\practica\target\debug\deps\practica-f7e7f87b40f7ced6.d: src\main.rs src\entregable_1\mod.rs src\entregable_1\juan_manuel_vila_v1.rs
+C:\Users\512ju\Desktop\facultad\INFORMATICA-APU\2024\Rust\practica\target\debug\deps\practica-f7e7f87b40f7ced6.d: src\main.rs src\tp03\mod.rs src\tp03\ej01.rs src\tp03\ej02.rs src\tp03\ej03.rs src\tp03\ej04.rs src\tp03\ej05.rs src\tp03\ej06.rs src\tp03\ej07.rs
 
 src\main.rs:
-src\entregable_1\mod.rs:
-src\entregable_1\juan_manuel_vila_v1.rs:
+src\tp03\mod.rs:
+src\tp03\ej01.rs:
+src\tp03\ej02.rs:
+src\tp03\ej03.rs:
+src\tp03\ej04.rs:
+src\tp03\ej05.rs:
+src\tp03\ej06.rs:
+src\tp03\ej07.rs:
