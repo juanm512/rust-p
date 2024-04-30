a5213429653ea29b0e0cdba94f2b16d24b02528d
juanm512 <512juanm@gmail.com>
2024-04-30 13:09:24 -0300

    EJERCICIO 05 casi terminado. Faltan los test

--- src/tp03/ej05.rs
+++ src/tp03/ej05.rs
@@ -1,27 +1,64 @@
-// 5-Defina la función llamada duplicar_valores que recibe un arreglo de números flotantes y
-// retorna un arreglo nuevo con los valores duplicados del parámetro.
+// 5- Escribir un programa que defina una estructura Producto que tenga campos para el
+// nombre, el precio bruto y un número identificatorio. Para dicha estructura implemente los
+// siguientes métodos:
+// ➢ new: que pasando los parámetros correspondientes, crea un Producto y lo retorna.
+// ➢ calcular_impuestos(porcentaje_de_impuestos): retorna el valor de impuestos sobre
+// el precio bruto
+// ➢ aplicar_descuento(porcentaje_de_descuento): retorna el valor del porcentaje de
+// descuento sobre el precio bruto
+// ➢ calcular_precio_total(porcentaje_de_impuestos, porcentaje_descuento): retorna el
+// precio total a pagar aplicando impuesto y descuento. Tenga en cuenta que los
+// parámetros son opcionales.
 
-pub fn run(){
-    let mut array: [f32; 8] = [1.94, 24.8, 32.5, 14.0, 5.0, 6.23, 27.1, 85.6];
-    println!("El arreglo original es {:?}", array);
-    
-    array = *duplicar_valores(&mut array);
-    println!("El nuevo arreglo es {:?}", array);
+
+#[derive(Debug)]
+struct Producto{
+    id: String,
+    nombre: String,
+    precio_bruto: f32,
 }
 
-fn duplicar_valores( numeros: & mut [f32; 8] ) -> &[f32; 8]{
-    for i in 0..numeros.len()   {
-        numeros[i] = numeros[i]*2.0;
+impl Producto{
+    pub fn new(id: String, nombre: String, precio_bruto: f32) -> Producto{
+        Producto{
+            id, nombre, precio_bruto
+        }
     }
+    pub fn calcular_impuestos(&self, porcentaje_de_impuestos: f32) -> f32 {
+        (porcentaje_de_impuestos / 100.0) * self.precio_bruto
+    }
+    pub fn aplicar_descuento(&self, porcentaje_de_descuento: f32) -> f32 {
+        (porcentaje_de_descuento / 100.0) * self.precio_bruto
+    }
+    pub fn calcular_precio_total(&self, porcentaje_de_impuestos: Option<f32>, porcentaje_descuento: Option<f32>) -> f32 {
+        let mut precio_total = self.precio_bruto;
+        
+        if let Some(data) = porcentaje_de_impuestos {
+            precio_total += self.calcular_impuestos(data);
+        }
 
-   numeros
+        if let Some(data2) = porcentaje_descuento {
+            precio_total -= self.aplicar_descuento(data2);
+        }
+
+        precio_total
+    }
 }
 
-#[test]
-fn test_duplicar_valores(){
-    let mut array: [f32; 8] = [1.94, 24.8, 32.5, 14.0, 5.0, 6.23, 27.1, 85.6];
-    array = *duplicar_valores(&mut array);
 
-    let array_duplicado: [f32; 8] = [3.88, 49.6, 65.0, 28.0, 10.0, 12.46, 54.2, 171.2];
-    assert_eq!(array, array_duplicado);
-}
+// pub fn run(){
+//     let id = String::from("");    
+//     let nombre = String::from("");    
+//     let precio: f32= 15.5;
+
+//     let p = Producto::new(id, nombre, precio);
+
+//     println!("{:#?}", p);
+//     println!("{:#?}", p.calcular_impuestos(10.0) );
+//     println!("{:#?}", p.aplicar_descuento(20.0) );
+//     println!("{:#?}", p.calcular_precio_total( Some(10.0), Some(30.0) ) );
+//     println!("{:#?}", p.calcular_precio_total( Some(10.0), None ) );
+// }
+
+
+// HACER TESTS
